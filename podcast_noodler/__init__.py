39c5db239c28dc7f2ccbd80e8ba2cd2a8eb9dd65
Simon Whitaker <sw@netcetera.org>
2024-05-11 18:10:19 +0100

    Define some asset jobs

--- podcast_noodler/__init__.py
+++ podcast_noodler/__init__.py
@@ -1,9 +1,21 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import Definitions, define_asset_job, load_assets_from_modules
 
 from . import assets
 
 all_assets = load_assets_from_modules([assets])
 
+all_assets_jobs = define_asset_job(name="all_assets_job")
+
+nlp_assets_jobs = define_asset_job(
+    name="nlp_assets_job",
+    selection=[
+        "episodes",
+        "most_frequent_tags",
+        "most_frequent_summary_words",
+    ],
+)
+
 defs = Definitions(
     assets=all_assets,
+    jobs=[all_assets_jobs, nlp_assets_jobs],
 )
