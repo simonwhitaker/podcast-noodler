eaf4984e193535153c81aca00577bd8f835789cc
Simon Whitaker <sw@netcetera.org>
2024-06-12 12:29:51 +0100

    Add OpenAI resource

--- podcast_noodler/__init__.py
+++ podcast_noodler/__init__.py
@@ -2,6 +2,7 @@ from dagster import Definitions, define_asset_job, load_assets_from_modules
 
 from .assets import episodes, stats
 from .jobs import audio_files_update_job
+from .resources import openai_resource
 
 episode_assets = load_assets_from_modules([episodes], group_name="episodes")
 stats_assets = load_assets_from_modules([stats], group_name="stats")
@@ -12,4 +13,5 @@ all_assets_jobs = define_asset_job(name="all_assets_job")
 defs = Definitions(
     assets=[*episode_assets, *stats_assets],
     jobs=[all_assets_jobs, audio_files_update_job],
+    resources={"openai": openai_resource},
 )
